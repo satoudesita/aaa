833e2e502f50ba78dc8d2549c74799a0daead24e
satoudesita <167180203+satoudesita@users.noreply.github.com>
2024-06-13 08:59:51 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,20 +14,17 @@ st.write('がんばってください！')
 # Load the data
 @st.cache
 def load_data():
-    return pd.read_excel("aaa.xlsx")
+    return 
 
-words_df = load_data()
+words_df = pd.read_excel("aaa.xlsx")
 
-st.write(load_data)
 
 
 # ガチャ機能
 if st.button('ガチャを引く！'):
     rarity_probs = {
-        'N': 0.4,
-        'R': 0.3,
-        'SR': 0.2,
-        'SSR': 0.1
+        'N': 0.5,
+        'R': 0.5
     }
     chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
     subset_df = words_df[words_df['レア度'] == chosen_rarity]
@@ -38,15 +35,15 @@ if st.button('ガチャを引く！'):
     st.session_state.display_meaning = False
 
 if 'selected_word' in st.session_state:
-    st.header(f"問題: {st.session_state.selected_word['問題']}")
+    st.header(f"単語: {st.session_state.selected_word['単語']}")
     st.subheader(f"レア度: {st.session_state.selected_word['レア度']}")
 
     # 意味を確認するボタンを追加
-    if st.button('解答を確認する'):
+    if st.button('意味を確認する'):
         st.session_state.display_meaning = True
 
     if st.session_state.display_meaning:
-        st.write(f"解答: {st.session_state.selected_word['解答']}")
+        st.write(f"意味: {st.session_state.selected_word['意味']}")
 
         
 
